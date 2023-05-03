7a1b7cf029a6a1a3b5216eda1ef63b06a66a703a
kkla320 <19488255+kkla320@users.noreply.github.com>
2023-05-03 14:27:10 +0200

    Added docc package

--- Package.swift
+++ Package.swift
@@ -20,13 +20,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ISO8601DurationFormatter",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "ISO8601DurationFormatterTests",
             dependencies: ["ISO8601DurationFormatter"]),
